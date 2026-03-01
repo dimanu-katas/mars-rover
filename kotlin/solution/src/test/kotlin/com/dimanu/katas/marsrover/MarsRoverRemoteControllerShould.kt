98ba362d3f209e93aa1f3bad6dd13723c9eec890
dimanu-py <dimanu.py@gmail.com>
2026-03-01 18:56:58 +0100

    test(remote-controller): verify that mars rover does not move when no command is provided

--- kotlin/solution/src/test/kotlin/com/dimanu/katas/marsrover/MarsRoverRemoteControllerShould.kt
+++ kotlin/solution/src/test/kotlin/com/dimanu/katas/marsrover/MarsRoverRemoteControllerShould.kt
@@ -1,20 +1,34 @@
 package com.dimanu.katas.marsrover
+import io.mockk.every
 import io.mockk.mockk
+import io.mockk.mockkClass
+import io.mockk.verify
+import org.junit.jupiter.api.BeforeEach
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
 class MarsRoverRemoteControllerShould {
     companion object {
         private const val NO_COMMAND = ""
+        private const val INITIAL_POSITION = "0:0:N"
+    }
+
+    private lateinit var marsRover: MarsRover
+    private lateinit var remoteController: MarsRoverRemoteController
+
+    @BeforeEach
+    fun setUp() {
+        marsRover = mockk<MarsRover>(relaxUnitFun = true)
+        remoteController = MarsRoverRemoteController(marsRover)
     }
 
     @Test
     fun `not move mars rover if no command is introduced`() {
-        val marsRover = mockk<MarsRover>()
-        val remoteController = MarsRoverRemoteController(marsRover)
+        every { marsRover.position() } returns INITIAL_POSITION
 
         val position = remoteController.execute(NO_COMMAND)
 
-        assertEquals("0:0:N", position)
+        assertEquals(INITIAL_POSITION, position)
+        verify { marsRover.doNothing() }
     }
 }
