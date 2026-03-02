6fa2af2411d7e01e0d3868f289449323a498cc7e
dimanu-py <dimanu.py@gmail.com>
2026-03-02 08:47:33 +0100

    refactor(command): implement Command interface in NoCommand class

--- kotlin/solution/src/main/kotlin/com/dimanu/katas/marsrover/NoCommand.kt
+++ kotlin/solution/src/main/kotlin/com/dimanu/katas/marsrover/NoCommand.kt
@@ -1,7 +1,7 @@
 package com.dimanu.katas.marsrover
 
-class NoCommand(private val marsRover: MarsRover) {
-    fun execute() {
+class NoCommand(private val marsRover: MarsRover): Command {
+    override fun execute() {
         marsRover.doNothing()
     }
 
