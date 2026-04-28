9ba12fdaf9d0874f92ece49ca6a82f942a6395cd
dimanupy <dimanu.py@gmail.com>
2026-04-28 16:13:44 +0200

    feat(mars-rover): create enum representing Orientation and encapsulate there the behavior in charge of knowing how rotation works

---
+++ kotlin/solution/src/main/kotlin/com/dimanu/katas/marsrover/Orientation.kt
@@ -0,0 +1,23 @@
+package com.dimanu.katas.marsrover
+
+enum class Orientation(private var value: String) {
+    NORTH(value = "N"), EAST(value = "E"), SOUTH(value = "S"), WEST(value = "W");
+
+    fun turnRight(): Orientation = when (this) {
+        NORTH -> EAST
+        EAST -> SOUTH
+        SOUTH -> WEST
+        WEST -> NORTH
+    }
+
+    fun turnLeft(): Orientation = when (this) {
+        NORTH -> WEST
+        EAST -> NORTH
+        SOUTH -> EAST
+        WEST -> SOUTH
+    }
+
+    override fun toString(): String {
+        return value
+    }
+}
